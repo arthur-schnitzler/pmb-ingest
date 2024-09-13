c2d47bc258bad491bf54b8435df75925d00dd249
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-13 13:50:18 +0200

    minor

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,9 +3,9 @@ FILES=("listbibl.xml" "listplace.xml" "listperson.xml" "listevent.xml" "listorg.
 URL=https://pmb.acdh.oeaw.ac.at/media/
 
 # for item in ${FILES[@]}
-   # do 
-   # rm to_ingest/${item}
-   # wget ${URL}${item} -O to_ingest/${item}
+#    do 
+#    rm to_ingest/${item}
+#    wget ${URL}${item} -O to_ingest/${item}
 # done
 
 wget "https://pmb.acdh.oeaw.ac.at/static/img/project-logo.svg" -O to_ingest/title-image.svg

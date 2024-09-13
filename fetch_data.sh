28ff7f9b198eed8b8ebc04080ded1b0d843c1886
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2024-09-13 13:40:57 +0200

    Update fetch_data.sh

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,10 +2,10 @@
 FILES=("listbibl.xml" "listplace.xml" "listperson.xml" "listevent.xml" "listorg.xml")
 URL=https://pmb.acdh.oeaw.ac.at/media/
 
-for item in ${FILES[@]}
-    do 
-    rm to_ingest/${item}
-    wget ${URL}${item} -O to_ingest/${item}
-done
+# for item in ${FILES[@]}
+   # do 
+   # rm to_ingest/${item}
+   # wget ${URL}${item} -O to_ingest/${item}
+# done
 
 wget "https://pmb.acdh.oeaw.ac.at/static/img/project-logo.svg" -O to_ingest/title-image.svg

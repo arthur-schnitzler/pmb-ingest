8e73000c53eb04d9599226a15a431f6ad6bd34d5
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-18 11:54:18 +0200

    closes #2;

--- fetch_data.sh
+++ fetch_data.sh
@@ -6,6 +6,9 @@ for item in ${FILES[@]}
    do 
    rm to_ingest/${item}
    wget ${URL}${item} -O to_ingest/${item}
+   echo -e "<?xml version='1.0' encoding='utf-8'?>\n$(cat to_ingest/${item})" > to_ingest/${item}
 done
 
+rm to_ingest/relations.xml
+wget ${URL}relations.gexf -O to_ingest/relations.xml
 wget "https://pmb.acdh.oeaw.ac.at/static/img/project-logo.svg" -O to_ingest/title-image.svg
